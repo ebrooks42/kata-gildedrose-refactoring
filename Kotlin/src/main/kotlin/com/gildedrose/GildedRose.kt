bf473e8cb18d9ebda62a315f69b1ed4a47e566a9
Evan White <evan.douglas.white@gmail.com>
2022-08-27 17:01:33 -0500

    convert if-elses to when

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,26 +4,28 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                bumpNonExpiredBackstagePassQuality(item)
-            } else if (item.name == "Aged Brie") {
-                item.incrementQuality()
-            } else {
-                if (item.name != "Sulfuras, Hand of Ragnaros") {
-                    item.decrementQuality()
+            when (item.name) {
+                "Backstage passes to a TAFKAL80ETC concert" -> bumpNonExpiredBackstagePassQuality(item)
+                "Aged Brie" -> item.incrementQuality()
+                else -> {
+                    if (item.name != "Sulfuras, Hand of Ragnaros") {
+                        item.decrementQuality()
+                    }
                 }
             }
 
             item.updateSellIn()
 
             if (item.sellIn < 0) {
-                if (item.name == "Aged Brie") {
-                    item.incrementQuality()
-                } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                    item.quality = 0
-                } else {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.decrementQuality()
+                when (item.name) {
+                    "Aged Brie" -> item.incrementQuality()
+                    "Backstage passes to a TAFKAL80ETC concert" -> {
+                        item.quality = 0
+                    }
+                    else -> {
+                        if (item.name != "Sulfuras, Hand of Ragnaros") {
+                            item.decrementQuality()
+                        }
                     }
                 }
             }
