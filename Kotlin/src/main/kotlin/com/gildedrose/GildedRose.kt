d9c8684e5c9c5d74f90b93961c1594b5eec1616d
Evan White <evan.douglas.white@gmail.com>
2022-08-27 16:53:14 -0500

    make if checks positive for expired goods

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -19,16 +19,14 @@ class GildedRose(var items: Array<Item>) {
             item.updateSellIn()
 
             if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.name != "Sulfuras, Hand of Ragnaros") {
-                            item.decrementQuality()
-                        }
-                    } else {
-                        item.quality = 0
-                    }
+                if (item.name == "Aged Brie") {
+                    item.incrementQuality()
+                } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    item.quality = 0
                 } else {
-                    updateAgedBrieQuality(item)
+                    if (item.name != "Sulfuras, Hand of Ragnaros") {
+                        item.decrementQuality()
+                    }
                 }
             }
         }
@@ -43,10 +41,5 @@ class GildedRose(var items: Array<Item>) {
             item.incrementQuality()
         }
     }
-
-    private fun updateAgedBrieQuality(item: Item) {
-        item.incrementQuality()
-    }
-
 }
 
