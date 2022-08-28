620a1e83637a847a361c4503cc9594c3580cdd1d
Evan White <evan.douglas.white@gmail.com>
2022-08-28 08:14:58 -0500

    extract item quality update to be owned by item

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,35 +4,7 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            when (item.name) {
-                "Backstage passes to a TAFKAL80ETC concert" -> bumpNonExpiredBackstagePassQuality(item)
-                "Aged Brie" -> item.incrementQualityButNoFurtherThanFifty()
-                "Sulfuras, Hand of Ragnaros" -> { /* quality does not decay */ }
-                else -> item.decrementQualityButNoFurtherThanZero()
-            }
-
-            item.updateSellIn()
-
-            if (item.sellIn < 0) {
-                when (item.name) {
-                    "Aged Brie" -> item.incrementQualityButNoFurtherThanFifty()
-                    "Backstage passes to a TAFKAL80ETC concert" -> item.quality = 0
-                    "Sulfuras, Hand of Ragnaros" -> { /* quality does not decay */ }
-                    else -> item.decrementQualityButNoFurtherThanZero()
-                }
-            }
-        }
-    }
-
-    private fun bumpNonExpiredBackstagePassQuality(item: Item) {
-        item.incrementQualityButNoFurtherThanFifty()
-
-        if (item.sellIn < 11) {
-            item.incrementQualityButNoFurtherThanFifty()
-        }
-
-        if (item.sellIn < 6) {
-            item.incrementQualityButNoFurtherThanFifty()
+            item.updateItemQuality()
         }
     }
 }
--- Kotlin/src/main/kotlin/com/gildedrose/Item.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/Item.kt
@@ -4,6 +4,41 @@ open class Item(var name: String, var sellIn: Int, var quality: Int) {
     override fun toString(): String {
         return this.name + ", " + this.sellIn + ", " + this.quality
     }
+    fun updateItemQuality() {
+        when (name) {
+            "Backstage passes to a TAFKAL80ETC concert" -> bumpNonExpiredBackstagePassQuality()
+            "Aged Brie" -> incrementQualityButNoFurtherThanFifty()
+            "Sulfuras, Hand of Ragnaros" -> { /* quality does not decay */
+            }
+
+            else -> decrementQualityButNoFurtherThanZero()
+        }
+
+        updateSellIn()
+
+        if (sellIn < 0) {
+            when (name) {
+                "Aged Brie" -> incrementQualityButNoFurtherThanFifty()
+                "Backstage passes to a TAFKAL80ETC concert" -> quality = 0
+                "Sulfuras, Hand of Ragnaros" -> { /* quality does not decay */
+                }
+
+                else -> decrementQualityButNoFurtherThanZero()
+            }
+        }
+    }
+
+    private fun bumpNonExpiredBackstagePassQuality() {
+        incrementQualityButNoFurtherThanFifty()
+
+        if (sellIn < 11) {
+            incrementQualityButNoFurtherThanFifty()
+        }
+
+        if (sellIn < 6) {
+            incrementQualityButNoFurtherThanFifty()
+        }
+    }
 
     fun updateSellIn() {
         if (name != "Sulfuras, Hand of Ragnaros") {
